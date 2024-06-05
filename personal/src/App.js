1f7e00026103ffb98ca58bed1b6b7dec924a5def
Jimmy Chavada <rchavada@uwo.ca>
2024-06-05 12:28:20 -0400

    Updated About page

--- personal/src/App.js
+++ personal/src/App.js
@@ -6,26 +6,16 @@ import Hero from "./components/Hero"
 import { useInView } from "react-intersection-observer";
 
 export default function App() {
-  const [isVisible, setVisible] = useState(true);
 
-  const domRef = useRef();
+  const aboutRef = useRef(null);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => entry.isIntersecting && setVisible(true));
-    });
-
-    observer.observe(domRef.current);
-
-    return () => observer.unobserve(domRef.current);
-  }, []);
 
   return (
     <div>
       <Nav />
       <div id='section-wrapper' className='hero-section'><Hero /></div>
       
-      <div id="section-wrapper" ref={domRef} className={`slide-in ${ isVisible ? 'is-visible' : '' } About-Section`}>
+      <div id="section-wrapper" ref={aboutRef} classname='About-Section'>
         <About />
       </div>
 
--- personal/src/components/About.js
+++ personal/src/components/About.js
@@ -1,58 +1,108 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-import { faReact, faPython, faCss3Alt} from '@fortawesome/free-brands-svg-icons';
-import { faDatabase} from "@fortawesome/free-solid-svg-icons"
-import React from 'react'
-import './css/about.css'
+import { React, useState, useRef, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact, faPython, faCss3Alt } from '@fortawesome/free-brands-svg-icons';
+import { faDatabase, faBrain } from "@fortawesome/free-solid-svg-icons";
+import headshot from "./images/headshot.JPG";
+import './css/about.css';
 
 export default function About() {
-    return (
-    <div className="shadow-2xl w-full max-w-10xl w3-container w3-center w3-animate-left w3-animate-opacity" style={{'opacity': 0.9, 'backgroundColor': '#254e58', 'padding': 20}}>
-      <h1 className='header text-center'>About Me</h1>
-      <div className="shadow-2xl py-24 sm:py-32 z-10 w-full max-w-7xl mx-auto" style={{'borderRadius': 20, 'opacity': 0.9, 'backgroundColor': '#112d32', 'padding': 20}}>
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-lg font-semibold leading-8 text-white" style={{'color': '#88bdbc'}}>
-            Technologies 
-            
-          </h2>
-          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-            <FontAwesomeIcon className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" icon={faReact} size="5x" style={{'color': '#88bdbc'}}/> 
-            <FontAwesomeIcon className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" icon={faPython} size="5x" style={{'color': '#88bdbc'}}/> 
-            <FontAwesomeIcon className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" icon={faCss3Alt} size="5x" style={{'color': '#88bdbc'}}/> 
-            <FontAwesomeIcon className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" icon={faDatabase} size="5x" style={{'color': '#88bdbc'}}/> 
-            <FontAwesomeIcon className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" icon={faReact} size="5x" style={{'color': '#88bdbc'}}/> 
-            <img
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-              alt="Reform"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-              alt="Tuple"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-              alt="SavvyCal"
-              width={158}
-              height={48}
-            />
-            <img
-              className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-              alt="Statamic"
-              width={158}
-              height={48}
-            />
+  const aboutRef = useRef(null);
+  const techRef = useRef(null);
+  const [isAboutIntersecting, setIsAboutIntersecting] = useState(false);
+  const [isTechIntersecting, setIsTechIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.target === aboutRef.current) {
+          setIsAboutIntersecting(entry.isIntersecting);
+        }
+        if (entry.target === techRef.current) {
+          setIsTechIntersecting(entry.isIntersecting);
+        }
+      },
+      { rootMargin: "0px" }
+    );
+
+    if (aboutRef.current) {
+      observer.observe(aboutRef.current);
+    }
+    if (techRef.current) {
+      observer.observe(techRef.current);
+    }
+
+    return () => {
+      if (aboutRef.current) {
+        observer.unobserve(aboutRef.current);
+      }
+      if (techRef.current) {
+        observer.unobserve(techRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <main>
+      <div className="shadow-2xl w-full max-w-10xl w3-container w3-center w3-animate-left w3-animate-opacity" style={{ opacity: 0.9, backgroundColor: '#254e58', padding: 100 }}>
+        <h1 className='header text-center'>About Me</h1>
+
+        <div
+          ref={aboutRef}
+          className={`aboutme shadow-2xl py-24 sm:py-32 z-10 w-full max-w-7xl mx-auto ${isAboutIntersecting ? 'slide-in' : ''}`}
+          style={{ borderRadius: 20, opacity: 0.9, backgroundColor: '#112d32', padding: 20 }}
+        >
+          <div className='about-content'>
+            <img src={headshot} alt='Rajan Headshot' className='headshot' />
+            <div className='text-content'>
+              <p>Hello! My name is Rajan Chavada, and I am a Computer Science student studying at the University of Western Ontario.</p>
+              <p>I am a passionate tech geek, and software developer. I've worked 8 Months at RBC Capital Markets as a software developer intern,
+                and have had the chance to meet a lot of cool people, and make enterprise software.
+              </p>
+              <p>I am most experienced in both front end and back end technologies with experience using React.js, SQL and Flask frameworks. I am currently learning
+                Datascience for machine learning, and plan to continue my education in this field!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          ref={techRef}
+          className={`technologies shadow-2xl py-24 sm:py-32 z-10 w-full max-w-7xl mx-auto ${isTechIntersecting ? 'slide-in' : ''}`}
+          style={{ borderRadius: 20, opacity: 0.9, backgroundColor: '#112d32', padding: 20 }}
+        >
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <h2 className="text-center text-lg font-semibold leading-8 text-white" style={{ color: '#88bdbc' }}>
+              Technologies
+            </h2>
+            <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+              <div className='tech'>
+                <FontAwesomeIcon className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" icon={faReact} size="5x" style={{ color: '#88bdbc' }} />
+                <p>React</p>
+              </div>
+
+              <div className='tech'>
+                <FontAwesomeIcon className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" icon={faPython} size="5x" style={{ color: '#88bdbc' }} />
+                <p>Python Lib</p>
+              </div>
+
+              <div className='tech'>
+                <FontAwesomeIcon className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" icon={faCss3Alt} size="5x" style={{ color: '#88bdbc' }} />
+                <p>CSS</p>
+              </div>
+
+              <div className='tech'>
+                <FontAwesomeIcon className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" icon={faDatabase} size="5x" style={{ color: '#88bdbc' }} />
+                <p>SQL</p>
+              </div>
+
+              <div className='tech'>
+                <FontAwesomeIcon className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" icon={faBrain} size="5x" style={{ color: '#88bdbc' }} />
+                <p>Machine Learning</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-    )
-  }
-  
+    </main>
+  );
+}
